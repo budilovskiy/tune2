2f5b6be0baaaa378455ea7ac024e644a8aab5bf3
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-08-04 19:56:30 +0300

    Update TrackURLFinder.java

--- src/com/felixfeatures/playlist/TrackURLFinder.java
+++ src/com/felixfeatures/playlist/TrackURLFinder.java
@@ -1,44 +1,43 @@
 package com.felixfeatures.playlist;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import com.felixfeatures.util.JSONUtil;
 
 /**
- * Provides static methods that perform search of the URL of tracks location at
- * vk.com using API
+ * Provides static methods to perform search of the URL of tracks location at
+ * vk.com using API.
+ * See VK API documentation https://vk.com/pages?oid=-1&p=audio.search
  */
 public class TrackURLFinder {
 
+	// Constant variable that holds search method value
 	private final static String VK_API_METHOD = "audio.search";
 	private final static String VK_API_TOKEN = "8***3";
 
 	/**
-	 * Builds and returns URL searching request of the given track to vk.com API.
-	 * See VK API documentation https://vk.com/pages?oid=-1&p=audio.search
+	 * Builds and returns URL searching request of the given track to vk.com
+	 * API.
 	 * 
 	 * @param searchQuery
 	 *            - full name of the track in "Artist - Name" String format
 	 * @return searching URL request to vk.com
 	 * @throws UnsupportedEncodingException
-	 * @throws MalformedURLException
+	 * @throws IOException
+	 *             from JSONUtil.parse()
 	 */
 	private static URL getVKRequestURL(String searchQuery)
 			throws UnsupportedEncodingException, MalformedURLException {
 		int averageRequestLength = 400;
 		// Building request URL to vk.com API service
-		StringBuilder sb = new StringBuilder(averageRequestLength); // 
+		StringBuilder sb = new StringBuilder(averageRequestLength); //
 		sb.append("https://api.vk.com/method/");
 		sb.append(VK_API_METHOD);
 		sb.append("?q=");
@@ -50,44 +49,29 @@ public class TrackURLFinder {
 		return new URL(sb.toString());
 	}
 
-	private static JSONArray parse(URL url) throws IOException, ParseException {
-
-		// Open connection
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-		// Receive JSON as String with search results
-		InputStream responseIS = connection.getInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				responseIS, "UTF-8"));
-		StringBuilder sb = new StringBuilder();
-
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			sb.append(line + "\n");
-		}
-		String result = sb.toString();
-
-		// Parse JSON to get track URL according to track duration
-		JSONParser parser = new JSONParser();
-		JSONObject jsonResponse = (JSONObject) parser.parse(result);
-		return (JSONArray) jsonResponse.get("response");
-	}
-
 	/**
 	 * Perform a request to vk.com, receive response in JSON format and parse it
 	 * to get URL of the first track in JSON document.
+	 * Have default access modifier to access it only within package
 	 * 
 	 * @param fullTrackName
 	 *            - String representation of track: "Artist - Name"
-	 * @return URL of the location of the given track
+	 * @return URL(in String) of the location of the given track
 	 * @throws IOException
-	 * @throws ParseException
 	 */
-	public static String getURLfromVK(String fullTrackName) throws IOException,
-			ParseException {
+	static String getURLfromVK(String fullTrackName) throws IOException {
 
 		URL requestURL = getVKRequestURL(fullTrackName);
-		JSONArray mp3list = (JSONArray) parse(requestURL);
+		
+		// Wait 1/3 second, because vk.com API has a limit of 3 requests in second
+		try {
+			Thread.sleep(350);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONArray mp3list = (JSONArray) JSONUtil.parse(requestURL);
 
 		JSONObject mp3 = (JSONObject) mp3list.get(1); // first track from VK response
 		return new String(mp3.get("url").toString());
@@ -96,20 +80,30 @@ public class TrackURLFinder {
 	/**
 	 * Perform a request to vk.com, receive response in JSON format and parse it
 	 * to get URL of the first track with given duration.
+	 * Have default access modifier to access it only within package
 	 * 
 	 * @param fullTrackName
 	 *            - String representation of track: "Artist - Name"
 	 * @param duration
 	 *            - duration of track
-	 * @return URL of the location of the given track with given duration
+	 * @return URL(in String) of the location of the given track with given duration
 	 * @throws IOException
-	 * @throws ParseException
+	 *             from JSONUtil.parse()
 	 */
-	public static String getURLfromVK(String fullTrackName, int duration)
-			throws IOException, ParseException {
+	static String getURLfromVK(String fullTrackName, int duration)
+			throws IOException {
 
 		URL requestURL = getVKRequestURL(fullTrackName);
-		JSONArray mp3list = (JSONArray) parse(requestURL);
+		
+		// Wait 1/3 second, because vk.com API has a limit of 3 requests in second
+		try {
+			Thread.sleep(350);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONArray mp3list = (JSONArray) JSONUtil.parse(requestURL);
 
 		String result = null;
 		for (int i = 1; i < mp3list.size(); i++) {
