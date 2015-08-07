ca3ce3eb73851b84506a06a6defd869fb39a1dbb
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-08-07 12:57:25 +0300

    Update AppGUI_draft.java

--- src/com/felixfeatures/GUI/AppGUI_draft.java
+++ src/com/felixfeatures/GUI/AppGUI_draft.java
@@ -1,1 +1,15 @@
 
+public class AppGUI_draft extends JFrame {
+  
+  private JLabel background; // background
+  private JTextField searchField; // search Field
+  private JLabel searchLabel; // search icon
+  private JTextField artistTF; // track artist info field
+  private JTextArea nameTF; // track name info field
+  private JTextField durationTF; // track duration info field
+  private JLabel stopLabel; // stop botton
+  private JLabel playLabel; // play button
+  private JLabel nextLabel; // next button
+  private JLabel imageLabel; // track image label
+  
+}
